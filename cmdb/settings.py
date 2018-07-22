4a6d102dfde26e06c1192df0ca50703f8edd7782
nikozhangwj <334743423@qq.com>
2018-07-22 10:56:21 +0800

    11

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0r(+64=aycra5rv*ae&rm%hj2#-tn6%2h%c@5_lz!tf!8&y@s+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
